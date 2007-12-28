9ca0f4f329d952ccd5b3309858fc3e3e32f0f8dc
Shannon Weyrick <weyrick@mozek.us>
2007-12-28 19:34:25 +0000

    ver

--- testSite/home/index.php
+++ testSite/home/index.php
@@ -19,7 +19,7 @@ require('../admin/common.inc');
 $layout1 = $SM_siteManager->rootTemplate("main.cpt");
 
 $intro  = '<B>SiteManager Test Suite</b><br><br>Click on the items in the left to test SiteManager functionality.<br><br>';
-$intro .= 'If all tests do not pass, check your PHP version (Zend PHP 5+ or Roadsend PHP 4+) and include file setup. If you need more help, head over to the
+$intro .= 'If all tests do not pass, check your PHP version (Zend PHP 5+ or Roadsend PHP 2.9.3+) and include file setup. If you need more help, head over to the
            <a href="http://code.roadsend.com" target="_new">Roadsend Community Site</a>.';
 
 // add intro
