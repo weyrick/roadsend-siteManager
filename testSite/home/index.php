4d15e5a814c2441eb96dd5fe8857b511e295a61e
Shannon Weyrick <weyrick@mozek.us>
2007-12-29 09:24:26 +0000

    show pcc ast cache stats

--- testSite/home/index.php
+++ testSite/home/index.php
@@ -22,6 +22,11 @@ $intro  = '<B>SiteManager Test Suite</b><br><br>Click on the items in the left t
 $intro .= 'If all tests do not pass, check your PHP version (Zend PHP 5+ or Roadsend PHP 2.9.3+) and include file setup. If you need more help, head over to the
            <a href="http://code.roadsend.com" target="_new">Roadsend Community Site</a>.';
 
+if (defined('ROADSEND_PCC')) {
+    $stats = pcc_memo_stats();
+    $intro .= "<br><br>Roadsend AST Cache Stats:<br>Hits: {$stats['hits']}<br>Misses: {$stats['misses']}<br>Resets: {$stats['resets']}<br>";
+}
+
 // add intro
 $layout1->addText($intro, 'main');
 
